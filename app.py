ed4cace22b0567ec572bf4023ad66071918f1659
ajith.g <ajith.g@mu-sigma.com>
2023-12-23 10:30:11 +0530

    BluePrint Registration

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 import logging
+import os
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 app = Flask(__name__)
+app.config.from_object(__name__)
+# app.config['HOSTED_PATH']=os.getenv(os.environ["FLASK_APP"],None)
+app.secret_key="FlaskAppSecrectKeyIamJocker"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+# db = SQLAlchemy(app)
+ma = Marshmallow(app)
+CORS(app)
+bcrypt = Bcrypt(app)
 
-@app.route("/")
-def hello_world():
-    app.logger.info('Info level log')
-    app.logger.warning('Warning level log')
-    return "<p>Hello, World!</p>"
 
-from view import routes
+from view.view import view
+app.register_blueprint(view)
 
 if __name__ == '__main__':
   app.run()
--- view/routes/routes.py
+++ view/routes/routes.py
@@ -1,8 +1,9 @@
-from app import app
 from flask import render_template
 import datetime
+from view.view import view
+from app import app
 
-@app.route("/view/index", methods=['GET'])
+@view.route("/index", methods=['GET'])
 def index():
     app.logger.info('Rendering Index page...!')
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
