11c467d85c14b0d5b8f7161fabd03c54d750c737
ajith.g <ajith.g@mu-sigma.com>
2023-12-28 13:42:41 +0530

    Finaziled dev commit

--- view/routes/routes.py
+++ view/routes/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, make_response, request, session
+from flask import render_template, make_response, request, session, url_for
 import datetime
 from view.view import view
 from app import app
+import os
 
 @view.route("/index", methods=['GET'])
 def index():
     app.logger.info('Rendering Index page...!')
-    render = make_response(render_template('index.html', utc_dt=datetime.datetime.utcnow()))
+    renderImage = url_for('view.static', filename = 'media/img/code.jpeg')
+    render = make_response(render_template('index.html', utc_dt=datetime.datetime.utcnow().strftime("%B %d %Y"), img_res = renderImage))
     render.set_cookie("Ajith-cookies", value="I am ajiths cookie..!")
     session['Ajith-session'] = "ajith-my-session"
     return render
